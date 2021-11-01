f37a7d14c47f4307f45f30bb52e31012bc4d71e8
Arun Ramaiah <78971492+aramaiah-ucd@users.noreply.github.com>
2021-11-01 06:18:48 -0700

    Add contact number on home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 //import logo from './logo.svg';
 import "./App.css";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import Home from "./components/Home";
 //import Products from "./components/Products";
@@ -19,78 +15,101 @@ import Pixxgen from "./components/Pixxgen";
 import SocialMedia from "./components/SocialMedia";
 import Construction from "./components/Construction";
 import Gelpad from "./components/Gelpad";
+import { FaPhoneAlt, MdOutlineMail } from "react-icons/fa";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-
-        <Navbar class="navbar-color" expand="lg">
         <div>
-          <img
-                  class="navbar-logo"
-                  src="../../assets/logos/indelect logo legacy.png"
-                  height="100px"
-                  alt="navbar-logo"
-                  href="/home"
-          ></img>
-        </div>
+          <Navbar class="navbar-color" expand="lg">
+            <span>
+              <img
+                class="navbar-logo"
+                src="../../assets/logos/indelect logo legacy.png"
+                height="100px"
+                alt="navbar-logo"
+                href="/home"
+              ></img>
+            </span>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-              <Nav.Link href="/home">Home</Nav.Link>
+                <Nav.Link href="/home">Home</Nav.Link>
                 <NavDropdown title="Products" id="basic-nav-dropdown">
                   <NavDropdown.Item href="/shimadzu">Shimadzu</NavDropdown.Item>
                   <NavDropdown.Item href="/genuine">Genuine</NavDropdown.Item>
                   <NavDropdown.Item href="/pixxgen">Pixxgen</NavDropdown.Item>
-                  <NavDropdown.Item href="/ctsp">Neuro Surgical Stereotactic Device(CTSP)</NavDropdown.Item>
-                  <NavDropdown.Item href="/gelpads">Surgical Instruments and Gel Pads</NavDropdown.Item>
+                  <NavDropdown.Item href="/ctsp">
+                    Neuro Surgical Stereotactic Device(CTSP)
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/gelpads">
+                    Surgical Instruments and Gel Pads
+                  </NavDropdown.Item>
                 </NavDropdown>
                 <Nav.Link href="/services">Services</Nav.Link>
                 <Nav.Link href="/customers">Customers</Nav.Link>
                 <Nav.Link href="/location">Location</Nav.Link>
                 <Nav.Link href="/about">About</Nav.Link>
-              </Nav>           
+              </Nav>
             </Navbar.Collapse>
           </Navbar>
-          <hr/>
-          <Switch>
-            <Route path="/shimadzu">
-              <Shimadzu />
-            </Route>
-            <Route path="/genuine">
-              <Genuine />
-            </Route>
-            <Route path="/pixxgen">
-              <Pixxgen />
-            </Route>
-            <Route path="/ctsp">
-              <Construction />
-            </Route>
-            <Route path="/gelpads">
-              <Gelpad />
-            </Route>
-            <Route path="/services">
-              <Services />
-            </Route>
-            <Route path="/customers">
-              <Customers />
-            </Route>
-            <Route path="/location">
-              <Location />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-          <hr/>
-      <footer class="page-footer">
-      <SocialMedia/>
-      </footer>
+        </div>
+        <div class="header-contact">
+          <div>
+          <span class="space-around">
+            <FaPhoneAlt />
+          </span>
+          <span class="space-around">Sales</span>
+          <span> + 91 93802 43402</span>
+          </div>
+          <div>
+          <span class="space-around">
+            <FaPhoneAlt />
+          </span>
+          <span>Service</span>
+          <span> + 91 93825 43299</span>
+        </div>
+        </div> 
       </div>
+      <hr />
+      <Switch>
+        <Route path="/shimadzu">
+          <Shimadzu />
+        </Route>
+        <Route path="/genuine">
+          <Genuine />
+        </Route>
+        <Route path="/pixxgen">
+          <Pixxgen />
+        </Route>
+        <Route path="/ctsp">
+          <Construction />
+        </Route>
+        <Route path="/gelpads">
+          <Gelpad />
+        </Route>
+        <Route path="/services">
+          <Services />
+        </Route>
+        <Route path="/customers">
+          <Customers />
+        </Route>
+        <Route path="/location">
+          <Location />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <hr />
+      <footer class="page-footer">
+        <SocialMedia />
+      </footer>
     </Router>
   );
 }
