f688df7baa83472acbcc28bacd614296bf35b2ca
arunramaiah <arunramaiah@users.noreply.github.com>
2023-04-11 00:40:35 -0700

    General fixes
    Fix logo spacing
    Fix spacing around map and contact details
    Modify about to use React vertical timeline (wip)

--- src/components/About.js
+++ src/components/About.js
@@ -4,18 +4,17 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css';
 
 const About = () => {
-  console.log(data);
   return (
     <div class="bg-color">
       <p class="main-heading">About Us</p>
       <p class="paragraph">
-        Indelect Technologies Pvt.Ltd. established in the year 1995 by the
+        Indelect Technologies Pvt.Ltd. established in the year 1995 by
         Technocrats having over 18 years of combined expertise in technology and
         mentoring experience in advanced areas of Biomedical Engineering,
-        Medical Imaging Technology & Power Electronics.Indelect, over these
+        Medical Imaging Technology & Power Electronics. Indelect, over these
         years has acclaimed a high reputation in the international markets apart
-        from serving the domestic Indian market. With a sustainable and
-        consistently growing progress, Indelect Technologies Pvt. Ltd. has
+        from serving the domestic Indian market. With sustainable and
+        consistent progress, Indelect Technologies Pvt. Ltd. has
         evolved into a dominant player in the distribution of Biomedical,
         Medical Imaging & Power Electronic equipments. The organization has
         expanded vastly in its length and breadth and has its foot firmly
@@ -43,14 +42,12 @@ const About = () => {
       {data.map((item, index) => {
         return (
           <div>
-            <VerticalTimeline>
-              <VerticalTimelineElement
-                className="vertical-timeline-element--work"
+            <VerticalTimeline className='floater'>
+              <VerticalTimelineElement 
                 date={item.year}
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+
               >
-                <h3 className="vertical-timeline-element-title"></h3>
-                <h4 className="vertical-timeline-element-subtitle"></h4>
                 <p>
                   {item.description}
                 </p>
